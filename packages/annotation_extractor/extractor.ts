3eb49598a2f42b1c6b01e9aa71a013a583cf7c3e
Doug Parker <dgp1130@users.noreply.github.com>
2020-12-27 22:34:14 -0800

    Adds `extractor` library.
    This parses an HTML string, identifies all the annotations, removes those comments, and then returns the new content with the parsed annotations.
    
    There are two tricky parts here. First is removing the annotation comments from the document. The `remove()` API does not exist for comment nodes, so instead we need to call `removeChild()` on the parent, which requires a lot more bookkeeping than we should really have to do. The second tricky part is deduplicating annotations. We want to deduplicate the parsed annotation so minor string edits don't introduce semantically equivalent elements. This required a deep equals implementation which is historically painful to do in JavaScript. Fortunately, there is an existing Node API I was able to leverage to minimize the maintenance overhead.
    
    I actually tried using the `jsdom` library initially, however it seems to have a bug in parsing child nodes of the body element, where I was not able to read comments under the `<body />`. `node-html-parser` seems like a lighter weight alternative anyways which is powerful enough for the requirements here.

---
+++ packages/annotation_extractor/extractor.ts
@@ -0,0 +1,104 @@
+import { parse, HTMLElement, Node, CommentNode } from 'node-html-parser';
+import { PrerenderAnnotation, parseAnnotation } from 'rules_prerender/common/prerender_annotation';
+import * as assert from 'assert';
+
+/**
+ * Parses the given string as HTML and return a tuple of the input HTML with
+ * annotation comments removed and a list of those parsed annotations.
+ * 
+ * @param html The HTML string to parse.
+ * @returns A tuple of two elements. The first element is the input HTML with
+ *     all annotation comments removed. The second element is a list of
+ *     parsed annotations found within the input HTML.
+ */
+export function extract(html: string): [
+    string /* strippedHtml */,
+    Set<PrerenderAnnotation> /* annotations */,
+] {
+    const root = parse(html, {
+        comment: true,
+        blockTextElements: {
+            script: true,
+            noscript: true,
+            style: true,
+            pre: true,
+        },
+    });
+
+    // Read all annotations from the HTML **and** remove the comments they come
+    // from. This actually applies a side effect of modifying the document.
+    const annotations = unique(stripAnnotations(walkComments(walk(root))));
+
+    return [ root.toString(), annotations ];
+}
+
+/**
+ * Checks each given comment to see if it is an annotation. If so, the comment
+ * is removed from the document and the annotation is yielded. Otherwise, the
+ * comment is left alone and nothing is emitted.
+ */
+function* stripAnnotations(
+    comments: Iterable<[ CommentNode, HTMLElement | undefined /* parent */ ]>,
+): Iterable<PrerenderAnnotation> {
+    for (const [ comment, parent ] of comments) {
+        const annotation = parseAnnotation(comment.text);
+        if (!annotation) continue;
+        if (!parent) throw new Error('Found comment node with no parent.');
+        parent.removeChild(comment);
+        yield annotation;
+    }
+}
+
+/**
+ * Filters the given {@link Iterable} of {@link Node} to limit to only comment
+ * nodes.
+ */
+function* walkComments(
+    nodes: Iterable<[ Node, HTMLElement | undefined /* parent */ ]>,
+): Iterable<[ CommentNode, HTMLElement | undefined /* parent */ ]> {
+    for (const [ node, parent ] of nodes) {
+        if (node instanceof CommentNode) {
+            yield [ node, parent ];
+        }
+    }
+}
+
+/**
+ * Returns an {@link Iterable} of all the {@link Node} descendants of the given
+ * root and their parent.
+ */
+function* walk(root: Node, parent?: HTMLElement):
+        Iterable<[ Node, HTMLElement | undefined /* parent */ ]> {
+    yield [ root, parent ];
+    if (root instanceof HTMLElement) {
+        for (const node of root.childNodes) {
+            yield* walk(node, root);
+        }
+    }
+}
+
+/**
+ * Returns a {@link Set} of the unique items of the given iterable, performing a
+ * deep equality comparison on the items to determine uniqueness.
+ */
+function unique<T extends object>(items: Iterable<T>): Set<T> {
+    const set = new Set<T>();
+    for (const item of items) {
+        const exists = Array.from(set.values())
+                .some((existing) => deepEqual(item, existing));
+        if (!exists) set.add(item);
+    }
+    return set;
+}
+
+/**
+ * Returns whether or not the two objects as deeply equivalent to each other.
+ */
+function deepEqual(first: object, second: object): boolean {
+    try {
+        assert.deepStrictEqual(first, second);
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
---
+++ packages/annotation_extractor/extractor_test.ts
@@ -0,0 +1,138 @@
+import 'jasmine';
+
+import { extract } from 'rules_prerender/packages/annotation_extractor/extractor';
+
+describe('extractor', () => {
+    describe('extract()', () => {
+        it('extracts annotations from the given HTML contents', () => {
+            const [ extracted, annotations ] = extract(`
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <title>Title</title>
+                        <!-- bazel:rules_prerender:PRIVATE_DO_NOT_DEPEND_OR_ELSE - {"type":"script","path":"foo/bar/baz.js"} -->
+                    </head>
+                    <body>
+                        <h2>Hello, World!</h2>
+                    </body>
+                </html>
+            `);
+
+            expect(extracted).toBe(`
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <title>Title</title>
+                        
+                    </head>
+                    <body>
+                        <h2>Hello, World!</h2>
+                    </body>
+                </html>
+            `);
+            expect(Array.from(annotations.values())).toEqual([
+                {
+                    type: 'script',
+                    path: 'foo/bar/baz.js',
+                },
+            ]);
+        });
+
+        it('extracts a first node annotation', () => {
+            const [ extracted, annotations ] = extract(`<!-- bazel:rules_prerender:PRIVATE_DO_NOT_DEPEND_OR_ELSE - {"type":"script","path":"foo/bar/baz.js"} -->
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <title>Title</title>
+                    </head>
+                    <body></body>
+                </html>
+            `);
+
+            expect(extracted).toBe(`
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <title>Title</title>
+                    </head>
+                    <body></body>
+                </html>
+            `);
+            expect(Array.from(annotations.values())).toEqual([
+                {
+                    type: 'script',
+                    path: 'foo/bar/baz.js',
+                },
+            ]);
+        });
+
+        it('ignores unrelated comments', () => {
+            const [ extracted, annotations ] = extract(`
+                <!-- Some leading comment. -->
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <!-- Another comment -->
+                        <title>Title</title>
+                    </head>
+                    <!-- One more comment. -->
+                    <body>
+                        <!-- bazel:rules_prerender:PRIVATE_DO_NOT_DEPEND_OR_ELSE - {"type":"script","path":"foo/bar/baz.js"} -->
+                    </body>
+                </html>
+            `);
+
+            expect(extracted).toBe(`
+                <!-- Some leading comment. -->
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <!-- Another comment -->
+                        <title>Title</title>
+                    </head>
+                    <!-- One more comment. -->
+                    <body>
+                        
+                    </body>
+                </html>
+            `);
+            expect(Array.from(annotations.values())).toEqual([
+                {
+                    type: 'script',
+                    path: 'foo/bar/baz.js',
+                },
+            ]);
+        });
+
+        it('deduplicates repeated annotations', () => {
+            const [ , annotations ] = extract(`
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <title>Title</title>
+                    </head>
+                    <body>
+                        <!-- Repeated annotations. -->
+                        <!-- bazel:rules_prerender:PRIVATE_DO_NOT_DEPEND_OR_ELSE - {"type":"script","path":"foo/bar/baz.js"} -->
+                        <!-- bazel:rules_prerender:PRIVATE_DO_NOT_DEPEND_OR_ELSE - {"type":"script","path":"foo/bar/baz.js"} -->
+                        <!-- bazel:rules_prerender:PRIVATE_DO_NOT_DEPEND_OR_ELSE - {"type":"script","path":"foo/bar/baz.js"} -->
+
+                        <!-- bazel:rules_prerender:PRIVATE_DO_NOT_DEPEND_OR_ELSE - {"type":"script","path":"hello/world.js"} -->
+                        <!-- bazel:rules_prerender:PRIVATE_DO_NOT_DEPEND_OR_ELSE - {"type":"script","path":"hello/world.js"} -->
+                    </body>
+                </html>
+            `);
+
+            expect(Array.from(annotations.values())).toEqual([
+                {
+                    type: 'script',
+                    path: 'foo/bar/baz.js',
+                },
+                {
+                    type: 'script',
+                    path: 'hello/world.js',
+                },
+            ]);
+        });
+    });
+});
