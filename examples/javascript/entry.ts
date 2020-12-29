88db5894f62d5766dd0e1ca79b8dc1c8c8f5432f
Doug Parker <dgp1130@users.noreply.github.com>
2020-12-28 17:58:23 -0800

    Updates Rollup entry point to use workspace imports.
    This requires Rollup to use Node resolution and `link_workspace_root = True` so `node_modules/` is set up correctly. With this, we can use workspace imports within Rollup bundles.

--- examples/javascript/entry.ts
+++ examples/javascript/entry.ts
@@ -1,4 +1,4 @@
 // This file is manually authored for now, but should be auto-generated.
-import './script';
-import './component/component_script';
-import './transitive/transitive_script';
+import 'rules_prerender/examples/javascript/script';
+import 'rules_prerender/examples/javascript/component/component_script';
+import 'rules_prerender/examples/javascript/transitive/transitive_script';
