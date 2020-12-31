1cd4ba6edbe4c7f7b54ed09e0a42cd2b27acc8d7
Doug Parker <dgp1130@users.noreply.github.com>
2020-12-30 19:32:05 -0800

    Adds `useTempDir()` effect.
    This encapsulates the logic for setting up and tearing down a temporary directory for testing, mostly useful for testing Node binaries that read and write files.
    
    Unfortunately spying on `fs` doesn't really work, so I had to work around that with dependency injection. Otherwise the implementation is relatively straightforward.

---
+++ common/testing/temp_dir_test.ts
@@ -0,0 +1,44 @@
+import 'jasmine';
+
+import { env } from 'process';
+import { useTempDir } from 'rules_prerender/common/testing/temp_dir';
+import { EffectTester } from 'rules_prerender/common/testing/effect_tester';
+
+const originalTempDir = env['TEST_TMPDIR'];
+describe('temp_dir', () => {
+    beforeEach(() => {
+        delete env['TEST_TMPDIR'];
+    });
+
+    afterEach(() => {
+        env['TEST_TMPDIR'] = originalTempDir;
+    });
+
+    describe('useTempDir()', () => {
+        it('provides a temporary directory as an effect', async () => {
+            env['TEST_TMPDIR'] = 'test-tmpDir';
+
+            const mkdtemp = jasmine.createSpy('mkdtemp')
+                    .and.resolveTo('test-tmpDir/useTempDir-foo');
+            const rmdir = jasmine.createSpy('rmdir').and.resolveTo();
+
+            const tester = EffectTester.of(() => useTempDir({
+                testonly: {
+                    mkdtemp,
+                    rmdir,
+                },
+            }));
+
+            expect(mkdtemp).not.toHaveBeenCalled();
+            await tester.initialize();
+            expect(mkdtemp).toHaveBeenCalledOnceWith('test-tmpDir/useTempDir-');
+            
+            expect(tester.get()).toBe('test-tmpDir/useTempDir-foo');
+
+            expect(rmdir).not.toHaveBeenCalled();
+            await tester.cleanup();
+            expect(rmdir).toHaveBeenCalledOnceWith(
+                'test-tmpDir/useTempDir-foo', { recursive: true });
+        });
+    });
+});
