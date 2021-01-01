8465d6625877d5fad75a85b2df5742773054b045
Doug Parker <dgp1130@users.noreply.github.com>
2020-12-31 23:22:34 -0800

    Ignore empty bundle Rollup warning.
    While working on a styles example, there is no JavaScript involved, so this was failing. By ignoring this warning we still bundle JavaScript, it just results in an empty file.

--- packages/rules_prerender/rollup-default.config.js
+++ packages/rules_prerender/rollup-default.config.js
@@ -8,6 +8,14 @@ export default {
 
     // Fail the build on any warning.
     onwarn(warning) {
+        // Ignore some warnings we don't care about.
+        if (ignoredWarnings.includes(warning.code)) return;
+
         throw new Error(warning.message);
     },
 };
+
+const ignoredWarnings = [
+    // If no scripts are included, then the bundle will be empty.
+    'EMPTY_BUNDLE',
+];
