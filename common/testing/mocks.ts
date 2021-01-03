eea90af0361d3966f1b4a5bbeb77e961b5dd526e
Doug Parker <dgp1130@users.noreply.github.com>
2021-01-02 22:32:13 -0800

    Adds mocking utilities.
    This includes two useful features:
    1. `Public` - This limits a type to only its public properties to hide private implementation details. Particularly useful for mocks which need to cast to the real type but the compiler may reject it because of missing private properties that aren't important to the mock.
    2. `unmockedFunc()` - This is useful for creating placeholder functions unrelated to a particular test but needed to satisfy the contract of a type. Particularly useful as a default for mocks of complex class methods that should always be spied on wherever they are needed.

---
+++ common/testing/mocks.ts
@@ -0,0 +1,39 @@
+/** @fileoverview Utilities around mocking objects. */
+
+import 'jasmine';
+
+/**
+ * Returns an object type containing only the publicly available properties of
+ * the input. This is useful because TypeScript interfaces include private
+ * properties, which can be annoying for mocking. Using this type filters out
+ * those private properties to stay consistent with the public API contract of a
+ * given type.
+ */
+export type Public<T> = { [K in keyof T]: T[K] };
+
+/** Holds all the unmocked functions by their name. */
+const unmockedFuncs = new Map<string, () => never>();
+
+/**
+ * Returns a function to serve as a placeholder mock for an unused function.
+ * Fails the test if it is ever called, because it should never actually be
+ * called. Returns the same function if called multiple times with the same
+ * name, that way using `expect().toEqual()` on objects with unmocked functions
+ * will not fail the test.
+ * 
+ * @param name The name of the function to use in error messaging.
+ * @return A function which will fail the test if called, with an error
+ *     referencing the provided name.
+ */
+export function unmockedFunc(name: string): (...args: unknown[]) => never {
+    const fn = unmockedFuncs.get(name);
+    if (fn) return fn;
+
+    const unmocked = (...args: unknown[]) => {
+        return fail(`Called unmocked function: \`${name}()\` with args:\n${
+            JSON.stringify(args, null /* replacer */, 4 /* tabSize */)
+        }.`) as never;
+    };
+    unmockedFuncs.set(name, unmocked);
+    return unmocked;
+}
