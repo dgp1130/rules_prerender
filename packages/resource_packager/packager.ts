3bf5767e9a493084109a9f9fdb6997e2830794b4
Doug Parker <dgp1130@users.noreply.github.com>
2021-01-02 23:19:36 -0800

    Adds `packager` library.
    This is responsible for "packing" a set of resources. This is done by simply copying them to the desired destination directory at their corresponding URL paths.

---
+++ packages/resource_packager/packager.ts
@@ -0,0 +1,43 @@
+import * as fs from 'rules_prerender/common/fs';
+import { ResourceMap } from 'rules_prerender/packages/resource_packager/resource_map';
+
+/**
+ * Copies all the provided resources to their specified URL path under the given
+ * destination directory.
+ * 
+ * Example:
+ * 
+ * Given a {@param destDir} of `'bazel-out/pkg/output'` and a
+ * {@link ResourceMap} which lists:
+ *   /foo/bar/baz.html -> bazel-out/path/to/pkg/baz.html
+ *   /hello/world.txt -> bazel-out/path/to/other/pkg/test.ext
+ * `pack` would create the files:
+ *   bazel-out/pkg/output/foo/bar/baz.html (with content of bazel-out/path/to/pkg/baz.html)
+ *   bazel-out/pkg/hello/world.txt (with content of bazel-out/path/to/other/pkg/test.ext)
+ * 
+ * @param destDir The destination directory to copy all the resources to.
+ * @param resources A {@link ResourceMap} which maps the desired URL paths to
+ *     their source files.
+ */
+export async function pack(destDir: string, resources: ResourceMap):
+        Promise<void> {
+    // Make all the directories containing files.
+    const dirs = new Set(Array.from(resources.urlPaths())
+        // Drop paths at root, no directory to create.
+        .filter((urlPath) => urlPath.lastIndexOf('/') !== 0)
+        .map((urlPath) => urlPath.slice(1) // Drop leading `/`.
+            .split('/').slice(0, -1).join('/') // Drop final filename.
+        ));
+    await Promise.all(Array.from(dirs.values())
+        .map((dir) => fs.mkdir(`${destDir}/${dir}`, { recursive: true }))
+    );
+
+    // Copy all source files to their associated path under `destDir`.
+    await Promise.all(
+        Array.from(resources.entries())
+            .map(([ urlPath, fileRef ]) => [
+                fileRef, // Source.
+                `${destDir}/${urlPath.slice(1)}`, // Destination.
+            ]).map(([ src, dest ]) => fs.copyFile(src, dest)),
+    );
+}
---
+++ packages/resource_packager/packager_test.ts
@@ -0,0 +1,85 @@
+import 'jasmine';
+
+import * as fs from 'rules_prerender/common/fs';
+import { pack } from 'rules_prerender/packages/resource_packager/packager';
+import { ResourceMap } from 'rules_prerender/packages/resource_packager/resource_map';
+import { mockFileRef } from 'rules_prerender/packages/resource_packager/resource_map_mock';
+
+describe('packager', () => {
+    describe('pack()', () => {
+        it('packages the given resources', async () => {
+            const resources = ResourceMap.fromEntries(Object.entries({
+                '/foo/bar/baz.html': 'bazel-bin/path/to/pkg/baz.html',
+                '/hello/world.html': 'bazel-bin/path/to/pkg/test.html',
+            }));
+
+            spyOn(fs, 'mkdir').and.resolveTo();
+            spyOn(fs, 'copyFile').and.resolveTo();
+
+            await pack('dest', resources);
+
+            expect(fs.mkdir).toHaveBeenCalledWith('dest/foo/bar', {
+                recursive: true,
+            });
+            expect(fs.mkdir).toHaveBeenCalledWith('dest/hello', {
+                recursive: true,
+            });
+
+            expect(fs.copyFile).toHaveBeenCalledWith(
+                'bazel-bin/path/to/pkg/baz.html', 'dest/foo/bar/baz.html');
+            expect(fs.copyFile).toHaveBeenCalledWith(
+                'bazel-bin/path/to/pkg/test.html', 'dest/hello/world.html');
+        });
+
+        it('does nothing when given no resources', async () => {
+            spyOn(fs, 'mkdir');
+            spyOn(fs, 'copyFile');
+
+            await pack('dest', ResourceMap.fromEntries(Object.entries({})));
+
+            expect(fs.mkdir).not.toHaveBeenCalled();
+            expect(fs.copyFile).not.toHaveBeenCalled();
+        });
+
+        // Root output directory should already exist, having been created by
+        // Bazel before the action runs.
+        it('does not create the root directory', async () => {
+            spyOn(fs, 'mkdir');
+            spyOn(fs, 'copyFile').and.resolveTo();
+
+            const map = ResourceMap.fromEntries(Object.entries({
+                '/foo.txt': mockFileRef(),
+            }));
+
+            await pack('dest', map);
+
+            expect(fs.mkdir).not.toHaveBeenCalled();
+        });
+
+        it('fails when unable to make a directory', async () => {
+            const err = new Error('File system detached.');
+            spyOn(fs, 'mkdir').and.rejectWith(err);
+            spyOn(fs, 'copyFile');
+
+            const map = ResourceMap.fromEntries(Object.entries({
+                '/foo/bar/baz.html': mockFileRef(),
+            }));
+
+            await expectAsync(pack('dest', map)).toBeRejectedWith(err);
+            
+            expect(fs.copyFile).not.toHaveBeenCalled();
+        });
+
+        it('fails when unable to copy a file', async () => {
+            const err = new Error('File system detached.');
+            spyOn(fs, 'mkdir').and.resolveTo();
+            spyOn(fs, 'copyFile').and.rejectWith(err);
+
+            const map = ResourceMap.fromEntries(Object.entries({
+                '/foo/bar/baz.html': mockFileRef(),
+            }));
+
+            await expectAsync(pack('dest', map)).toBeRejectedWith(err);
+        });
+    });
+});
