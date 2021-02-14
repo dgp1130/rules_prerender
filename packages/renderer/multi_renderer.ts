a1324167bc2590b238797bd73b193e8d85c69ece
Doug Parker <dgp1130@users.noreply.github.com>
2021-02-13 18:59:28 -0800

    Adds multi-renderer tool.
    Refs #1.
    
    This tool is roughly equivalent to the existing renderer tool in purpose, however it allows user code to return `PrerenderResource` objects and will write all of them to disk under a single output directory. This effectively allows users to render multiple files at once rather than being limited to just one.
    
    This tool is exported to users, though it has the same awkward caveats as the existing renderer. Because we dynamically import user code, we must compile user code into the `nodejs_binary()` which runs the tool. As a result, we can't publish a `nodejs_binary()` directly, but must instead build one as needed, including both multi-renderer code and user code all in a single binary. This means the published version of the tool is not actually a `nodejs_binary()` as one might expect, but instead it is just a `filegroup()` of the code required to run the tool. For simplicity we "round up" to all the code in the package, since it makes little difference to users who have to install the whole package anyways and ensures we won't forget to list a critical file.

---
+++ packages/renderer/multi_renderer.ts
@@ -0,0 +1,76 @@
+import { promises as fs } from 'fs';
+import * as path from 'path';
+import * as yargs from 'yargs';
+import { main } from 'rules_prerender/common/binary';
+import { mdSpacing } from 'rules_prerender/common/formatters';
+import { PrerenderResource } from 'rules_prerender/common/models/prerender_resource';
+import { invoke } from 'rules_prerender/packages/renderer/entry_point';
+
+main(async () => {
+    // Parse binary options and arguments.
+    const {
+        'entry-point': entryPoint,
+        'output-dir': outputDir,
+    } = yargs
+        .option('entry-point', {
+            type: 'string',
+            description: mdSpacing(`
+                The entry point to render the template. Should be a path to a
+                JavaScript file which has a CommonJS default export of a
+                function which takes no arguments and returns a
+                \`Iterable<PrerenderResource>\`, a
+                \`Promise<Iterable<PrerenderResource>>\`, or an
+                \`AsyncIterable<PrerenderResource>\`.
+            `),
+        })
+        .option('output-dir', {
+            type: 'string',
+            description: mdSpacing(`
+                The path to the output directory to write the rendered files to
+                at their specified paths.
+            `),
+        })
+        .demand([ 'entry-point', 'output-dir' ])
+        .argv;
+
+    // Invoke the provided entry point.
+    let resources: string | Iterable<PrerenderResource>
+        | AsyncIterable<PrerenderResource>;
+    try {
+        resources = await invoke(entryPoint);
+    } catch (err) {
+        console.error(err.message);
+        return 1;
+    }
+
+    // Validate the result of the entry point.
+    if (typeof resources === 'string') {
+        console.error(
+            `Expected entry point (${entryPoint}) to return one of:\n${[
+                'Iterable<PrerenderResource>',
+                'Promise<Iterable<PrerenderResource>>',
+                'AsyncIterable<PrerenderResource>',
+            ].join('\n')}\n\nInstead, got:\n${JSON.stringify(resources)}`,
+        );
+        return 1;
+    }
+
+    // Write each resource to its file.
+    const writes = [] as Promise<void>[];
+    for await (const resource of resources) {
+        const urlPath = path.join(outputDir, resource.path);
+        const dir = urlPath.split('/').slice(0, -1).join('/');
+
+        // Don't block the loop on write, so I/O operations from multiple
+        // resources can run in parallel, instead just queue the Promise and
+        // `await` them all at the end.
+        writes.push((async () => {
+            await fs.mkdir(dir, { recursive: true });
+            await fs.writeFile(urlPath, new Uint8Array(resource.contents));
+        })());
+    }
+
+    await Promise.all(writes);
+
+    return 0;
+});
---
+++ packages/renderer/multi_renderer_test.ts
@@ -0,0 +1,194 @@
+import 'jasmine';
+
+import { promises as fs } from 'fs';
+import { resolveRunfile } from 'rules_prerender/common/runfiles';
+import { execBinary, ProcessResult } from 'rules_prerender/common/testing/binary';
+import { useTempDir } from 'rules_prerender/common/testing/temp_dir';
+
+const renderer = resolveRunfile(
+        'rules_prerender/packages/renderer/multi_renderer_test_binary.sh');
+
+/** Invokes the renderer binary. */
+async function run({ entryPoint, outputDir }: {
+    entryPoint: string,
+    outputDir: string,
+}): Promise<ProcessResult> {
+    return await execBinary(renderer, [
+        '--entry-point', entryPoint,
+        '--output-dir', outputDir,
+    ]);
+}
+
+describe('renderer', () => {
+    const tmpDir = useTempDir();
+
+    it('renders `Iterable<PrerenderResource>`', async () => {
+        await fs.mkdir(`${tmpDir.get()}/rendered`);
+        await fs.writeFile(`${tmpDir.get()}/foo.js`, `
+// We can't rely on the linker to resolve imports for us. We also don't want to
+// rely on the legacy require() patch, so instead we need to manually load the
+// runfiles helper and require() the file through it.
+const runfiles = require(process.env['BAZEL_NODE_RUNFILES_HELPER']);
+const { PrerenderResource } = require(runfiles.resolveWorkspaceRelative('common/models/prerender_resource.js'));
+
+module.exports = function* () {
+    yield PrerenderResource.of('/foo.html', 'foo');
+    yield PrerenderResource.of('/bar.html', 'bar');
+    yield PrerenderResource.of('/hello/world.html', 'Hello, World!');
+};
+        `.trim());
+
+        const { code, stdout, stderr } = await run({
+            entryPoint: `${tmpDir.get()}/foo.js`,
+            outputDir: `${tmpDir.get()}/output`,
+        });
+
+        expect(code).toBe(0, `Binary unexpectedly failed. STDERR:\n${stderr}`);
+        expect(stdout).toBe('');
+        expect(stderr).toBe('');
+
+        const foo = await fs.readFile(
+                `${tmpDir.get()}/output/foo.html`, 'utf8');
+        expect(foo).toBe('foo');
+
+        const bar = await fs.readFile(
+                `${tmpDir.get()}/output/bar.html`, 'utf8');
+        expect(bar).toBe('bar');
+            
+        const world = await fs.readFile(
+                `${tmpDir.get()}/output/hello/world.html`, 'utf8');
+        expect(world).toBe('Hello, World!');
+    });
+
+    it('renders `Promise<Iterable<PrerenderResource>>`', async () => {
+        await fs.mkdir(`${tmpDir.get()}/rendered`);
+        await fs.writeFile(`${tmpDir.get()}/foo.js`, `
+// We can't rely on the linker to resolve imports for us. We also don't want to
+// rely on the legacy require() patch, so instead we need to manually load the
+// runfiles helper and require() the file through it.
+const runfiles = require(process.env['BAZEL_NODE_RUNFILES_HELPER']);
+const { PrerenderResource } = require(runfiles.resolveWorkspaceRelative('common/models/prerender_resource.js'));
+
+module.exports = () => {
+    return function* () {
+        yield PrerenderResource.of('/foo.html', 'foo');
+        yield PrerenderResource.of('/bar.html', 'bar');
+        yield PrerenderResource.of('/hello/world.html', 'Hello, World!');
+    }();
+};
+        `.trim());
+
+        const { code, stdout, stderr } = await run({
+            entryPoint: `${tmpDir.get()}/foo.js`,
+            outputDir: `${tmpDir.get()}/output`,
+        });
+
+        expect(code).toBe(0, `Binary unexpectedly failed. STDERR:\n${stderr}`);
+        expect(stdout).toBe('');
+        expect(stderr).toBe('');
+
+        const foo = await fs.readFile(
+                `${tmpDir.get()}/output/foo.html`, 'utf8');
+        expect(foo).toBe('foo');
+
+        const bar = await fs.readFile(
+                `${tmpDir.get()}/output/bar.html`, 'utf8');
+        expect(bar).toBe('bar');
+            
+        const world = await fs.readFile(
+                `${tmpDir.get()}/output/hello/world.html`, 'utf8');
+        expect(world).toBe('Hello, World!');
+    });
+
+    it('renders `AsyncIterable<PrerenderResource>`', async () => {
+        await fs.mkdir(`${tmpDir.get()}/output`);
+        await fs.writeFile(`${tmpDir.get()}/foo.js`, `
+// We can't rely on the linker to resolve imports for us. We also don't want to
+// rely on the legacy require() patch, so instead we need to manually load the
+// runfiles helper and require() the file through it.
+const runfiles = require(process.env['BAZEL_NODE_RUNFILES_HELPER']);
+const { PrerenderResource } = require(runfiles.resolveWorkspaceRelative('common/models/prerender_resource.js'));
+
+module.exports = async function* () {
+    yield PrerenderResource.of('/foo.html', 'foo');
+    yield PrerenderResource.of('/bar.html', 'bar');
+    yield PrerenderResource.of('/hello/world.html', 'Hello, World!');
+};
+        `.trim());
+
+        const { code, stdout, stderr } = await run({
+            entryPoint: `${tmpDir.get()}/foo.js`,
+            outputDir: `${tmpDir.get()}/output`,
+        });
+
+        expect(code).toBe(0, `Binary unexpectedly failed. STDERR:\n${stderr}`);
+        expect(stdout).toBe('');
+        expect(stderr).toBe('');
+
+        const foo = await fs.readFile(
+                `${tmpDir.get()}/output/foo.html`, 'utf8');
+        expect(foo).toBe('foo');
+
+        const bar = await fs.readFile(
+                `${tmpDir.get()}/output/bar.html`, 'utf8');
+        expect(bar).toBe('bar');
+            
+        const world = await fs.readFile(
+                `${tmpDir.get()}/output/hello/world.html`, 'utf8');
+        expect(world).toBe('Hello, World!');
+    });
+
+    it('fails from import errors from the entry point', async () => {
+        await fs.mkdir(`${tmpDir.get()}/output`);
+        await fs.writeFile(`${tmpDir.get()}/foo.js`, `
+module.exports = 'Hello, World!'; // Not a function...
+        `.trim());
+
+        const { code, stdout, stderr } = await run({
+            entryPoint: `${tmpDir.get()}/foo.js`,
+            outputDir: `${tmpDir.get()}/output`,
+        });
+
+        expect(code)
+            .toBe(1, `Binary unexpectedly succeeded. STDERR:\n${stderr}`);
+        expect(stdout).toBe('');
+        expect(stderr).toContain(`${tmpDir.get()}/foo.js`);
+        expect(stderr).toContain(
+                'provided a default export that was not a function');
+        
+        // No output files should be written.
+        const outputFiles = await fs.readdir(`${tmpDir.get()}/output`, {
+            withFileTypes: true,
+        });
+        expect(outputFiles).toEqual([]);
+    });
+
+    it('fails from a string result from the entry point', async () => {
+        await fs.mkdir(`${tmpDir.get()}/output`);
+        await fs.writeFile(`${tmpDir.get()}/foo.js`, `
+// Return a value which is valid user code, but incompatible with multi-renderer
+// due to being a \`string\` or \`Promise<string>\`.
+module.exports = () => {
+    return 'Hello, World!';
+};
+        `.trim());
+
+        const { code, stdout, stderr } = await run({
+            entryPoint: `${tmpDir.get()}/foo.js`,
+            outputDir: `${tmpDir.get()}/output`,
+        });
+
+        expect(code).toBe(1);
+        expect(stdout).toBe('');
+        expect(stderr).toContain(`${tmpDir.get()}/foo.js`);
+        expect(stderr).toContain('return one of:');
+        // Stack trace should not be displayed for user-fault errors.
+        expect(stderr).not.toContain('    at ');
+        
+        // No output files should be written.
+        const outputFiles = await fs.readdir(`${tmpDir.get()}/output`, {
+            withFileTypes: true,
+        });
+        expect(outputFiles).toEqual([]);
+    });
+});
