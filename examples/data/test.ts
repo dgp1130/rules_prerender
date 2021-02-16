59c13cf2d1a434162151509fda3965079806df7b
Doug Parker <dgp1130@users.noreply.github.com>
2021-02-15 20:41:28 -0800

    Adds example which uses the `data` attribute.
    Refs #24.
    
    This stores several different posts in text files within the workspace. This example depends on those files via the `data` attribute and then reads them during rendering to render a new HTML page for each text file. It also generates an index page which links to all the post pages.

---
+++ examples/data/test.ts
@@ -0,0 +1,93 @@
+import 'jasmine';
+import { useDevserver } from 'rules_prerender/common/testing/devserver';
+import { resolveRunfile } from 'rules_prerender/common/runfiles';
+import { useBrowser, usePage } from 'rules_prerender/common/testing/puppeteer';
+
+const devserverBinary =
+    resolveRunfile('rules_prerender/examples/data/devserver');
+
+describe('data', () => {
+    const server = useDevserver(devserverBinary);
+    const browser = useBrowser();
+    const page = usePage(browser);
+
+    describe('index page', () => {
+        it('renders', async () => {
+            await page.get().goto(
+                `http://${server.get().host}:${server.get().port}/`,
+                { waitUntil: 'load' },
+            );
+
+            const title = await page.get().title();
+            expect(title).toBe('Data');
+
+            const header = await page.get().$eval('h2', (el) => el.textContent);
+            expect(header).toBe('Data');
+
+            const links = await page.get().$$eval(
+                'li > a', (els) => els.map((el) => el.getAttribute('href')));
+            expect(links).toEqual(jasmine.arrayWithExactContents([
+                '/posts/foo.html',
+                '/posts/bar.html',
+                '/posts/baz.html',
+            ]));
+        });
+    });
+
+    describe('foo page', () => {
+        it('renders', async () => {
+            await page.get().goto(
+                `http://${server.get().host}:${server.get().port}/posts/foo.html`,
+                { waitUntil: 'load' },
+            );
+
+            const title = await page.get().title();
+            expect(title).toBe('foo');
+
+            const header = await page.get().$eval('h2', (el) => el.textContent);
+            expect(header).toBe('foo');
+
+            const content = await page.get().$eval(
+                'article', (el) => el.textContent);
+            expect(content).toBe('This is the text for the "foo" post!');
+        });
+    });
+
+    describe('bar page', () => {
+        it('renders', async () => {
+            await page.get().goto(
+                `http://${server.get().host}:${server.get().port}/posts/bar.html`,
+                { waitUntil: 'load' },
+            );
+
+            const title = await page.get().title();
+            expect(title).toBe('bar');
+
+            const header = await page.get().$eval('h2', (el) => el.textContent);
+            expect(header).toBe('bar');
+
+            const content = await page.get().$eval(
+                'article', (el) => el.textContent);
+            expect(content).toBe('This is the text for the "bar" post!');
+        });
+    });
+
+    describe('baz page', () => {
+        it('renders', async () => {
+            await page.get().goto(
+                `http://${server.get().host}:${server.get().port}/posts/baz.html`,
+                { waitUntil: 'load' },
+            );
+
+            const title = await page.get().title();
+            expect(title).toBe('baz');
+
+            const header = await page.get().$eval('h2', (el) => el.textContent);
+            expect(header).toBe('baz');
+
+            const content = await page.get().$eval(
+                'article', (el) => el.textContent);
+            expect(content).toBe('This is the text for the "baz" post!');
+        });
+    });
+});
