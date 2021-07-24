40c38260776164052a66a96bd123b2b7cadb86fe
Doug Parker <dgp1130@users.noreply.github.com>
2021-07-24 14:44:16 -0700

    WIP: Adds Karma.

--- packages/rules_prerender/declarative_shadow_dom_polyfill.spec.ts
+++ packages/rules_prerender/declarative_shadow_dom_polyfill.spec.ts
@@ -1,9 +1,13 @@
+/// <amd-module name="rules_prerender/packages/rules_prerender/declarative_shadow_dom_polyfill.spec"/>
+
 import 'jasmine';
 
 describe('declarative_shadow_dom_polyfill', () => {
     describe('polyfillDeclarativeShadowDom()', () => {
         it('polyfills declarative shadow DOM', () => {
-            // TODO
+            expect(true).toBe(true); // DEBUG
         });
     });
 });
+
+export {}; // DEBUG
