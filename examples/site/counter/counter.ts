b94d45bab254338495aeb580be722bc6fe554263
Doug Parker <dgp1130@users.noreply.github.com>
2021-09-25 14:07:59 -0700

    Migrates `site` example's counter page to use declarative shadow DOM.
    This page doesn't have any custom styles, so this has minimal value, but good consistency.

--- examples/site/counter/counter.ts
+++ examples/site/counter/counter.ts
@@ -1,4 +1,5 @@
 import { PrerenderResource } from 'rules_prerender';
+import { polyfillDeclarativeShadowDom } from 'rules_prerender/declarative_shadow_dom';
 import { srcLink } from 'rules_prerender/examples/site/common/links';
 import { baseLayout } from 'rules_prerender/examples/site/components/base/base';
 import { renderCounter } from 'rules_prerender/examples/site/components/counter/counter_prerender';
@@ -6,22 +7,29 @@ import { renderCounter } from 'rules_prerender/examples/site/components/counter/
 /** Renders the counter page. */
 export default async function*():
         AsyncGenerator<PrerenderResource, void, void> {
-    yield PrerenderResource.of('/counter/index.html', await baseLayout('Counter', () => `
-        <article>
-            <p>This is a basic JavaScript counter.</p>
+    yield PrerenderResource.of(
+        '/counter/index.html',
+        await baseLayout('Counter', () => `
+<article>
+    <template shadowroot="open">
+        ${polyfillDeclarativeShadowDom()}
 
-            <section>
-                ${renderCounter(10)}
-            </section>
+        <p>This is a basic JavaScript counter.</p>
 
-            <p>The <a href="${srcLink('/examples/site/components/counter/')}" rel="noopener">counter</a>
-            is built as a
-            <a href="https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements" rel="noopener">custom element</a>
-            with prerendered DOM. The label and buttons are rendered at build
-            time, even with a default value of 10. JavaScript is loaded lazily
-            and the component
-            <a href="https://en.wikipedia.org/wiki/Hydration_(web_development)" rel="noopener">hydrates</a>
-            itself without requiring any client-side rendering.</p>
-        </article>
-    `));
+        <section>
+            ${renderCounter(10)}
+        </section>
+
+        <p>The <a href="${srcLink('/examples/site/components/counter/')}" rel="noopener">counter</a>
+        is built as a
+        <a href="https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements" rel="noopener">custom element</a>
+        with prerendered DOM. The label and buttons are rendered at build time,
+        even with a default value of 10. JavaScript is loaded lazily and the
+        component
+        <a href="https://en.wikipedia.org/wiki/Hydration_(web_development)" rel="noopener">hydrates</a>
+        itself without requiring any client-side rendering.</p>
+    </template>
+</article>
+        `),
+    );
 }
