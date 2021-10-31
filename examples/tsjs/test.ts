45a115165377af52eb7fa60bac7aa9457ab85df9
Doug Parker <dgp1130@users.noreply.github.com>
2021-10-30 18:49:15 -0700

    Migrates `tsjs` example test from Puppeteer to WebDriverIO.

--- examples/tsjs/test.ts
+++ examples/tsjs/test.ts
@@ -2,76 +2,49 @@ import 'jasmine';
 
 import { runfiles } from '@bazel/runfiles';
 import { useDevserver } from 'rules_prerender/common/testing/devserver';
-import { useBrowser, usePage, puppeteerTestTimeout } from 'rules_prerender/common/testing/puppeteer';
+import { useWebDriver, webDriverTestTimeout } from 'rules_prerender/common/testing/webdriver';
 
 const devserverBinary = runfiles.resolvePackageRelative('devserver');
 
 describe('TS/JS', () => {
-    const server = useDevserver(devserverBinary);
-    const browser = useBrowser();
-    const page = usePage(browser);
+    const devserver = useDevserver(devserverBinary);
+    const wd = useWebDriver(devserver);
 
     it('renders TypeScript depending on JavaScript', async () => {
-        await page.get().goto(
-            `http://${server.get().host}:${server.get().port}/ts-depends-on-js.html`,
-            { waitUntil: 'load' },
-        );
+        const browser = wd.get();
+        await browser.url('/ts-depends-on-js.html');
 
-        const title = await page.get().title();
-        expect(title).toBe('TS depends on JS');
-
-        const tsParent = await page.get().$eval(
-            '.ts-parent > span', (el) => el.textContent);
-        expect(tsParent).toBe('TS parent');
-
-        const jsChild = await page.get().$eval(
-            '.js-child', (el) => el.textContent?.trim());
-        expect(jsChild).toBe('JS child');
-    }, puppeteerTestTimeout);
+        expect(await browser.getTitle()).toBe('TS depends on JS');
+        expect(await browser.$('.ts-parent > span').getText())
+            .toBe('TS parent');
+        expect(await browser.$('.js-child').getText()).toBe('JS child');
+    }, webDriverTestTimeout);
 
     it('renders JavaScript depending on TypeScript', async () => {
-        await page.get().goto(
-            `http://${server.get().host}:${server.get().port}/js-depends-on-ts.html`,
-            { waitUntil: 'load' },
-        );
+        const browser = wd.get();
+        await browser.url('/js-depends-on-ts.html');
 
-        const title = await page.get().title();
-        expect(title).toBe('JS depends on TS');
-
-        const jsParent = await page.get().$eval(
-            '.js-parent > span', (el) => el.textContent);
-        expect(jsParent).toBe('JS parent');
-
-        const tsChild = await page.get().$eval(
-            '.ts-child', (el) => el.textContent?.trim());
-        expect(tsChild).toBe('TS child');
-    }, puppeteerTestTimeout);
+        expect(await browser.getTitle()).toBe('JS depends on TS');
+        expect(await browser.$('.js-parent > span').getText())
+            .toBe('JS parent');
+        expect(await browser.$('.ts-child').getText()).toBe('TS child');
+    }, webDriverTestTimeout);
 
     it('renders client-side TypeScript depending on JavaScript', async () => {
-        await page.get().goto(
-            `http://${server.get().host}:${server.get().port}/ts-script-depends-on-js-script.html`,
-            { waitUntil: 'load' },
-        );
+        const browser = wd.get();
+        await browser.url('/ts-script-depends-on-js-script.html');
 
-        const title = await page.get().title();
-        expect(title).toBe('TS script depends on JS script');
-
-        const replaced = await page.get().$eval(
-            '#replace-ts-parent-script', (el) => el.textContent);
-        expect(replaced).toBe('Hello, World!');
-    }, puppeteerTestTimeout);
+        expect(await browser.getTitle()).toBe('TS script depends on JS script');
+        expect(await browser.$('#replace-ts-parent-script').getText())
+            .toBe('Hello, World!');
+    }, webDriverTestTimeout);
 
     it('renders client-side JavaScript depending on TypeScript', async () => {
-        await page.get().goto(
-            `http://${server.get().host}:${server.get().port}/js-script-depends-on-ts-script.html`,
-            { waitUntil: 'load' },
-        );
+        const browser = wd.get();
+        await browser.url('/js-script-depends-on-ts-script.html');
 
-        const title = await page.get().title();
-        expect(title).toBe('JS script depends on TS script');
-
-        const replaced = await page.get().$eval(
-            '#replace-js-parent-script', (el) => el.textContent);
-        expect(replaced).toBe('Hello, World!');
-    }, puppeteerTestTimeout);
+        expect(await browser.getTitle()).toBe('JS script depends on TS script');
+        expect(await browser.$('#replace-js-parent-script').getText())
+            .toBe('Hello, World!');
+    }, webDriverTestTimeout);
 });
