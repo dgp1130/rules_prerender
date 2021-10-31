00e3cd3c6adce4eca7745ca71772bf074b17716e
Doug Parker <dgp1130@users.noreply.github.com>
2021-10-30 19:12:21 -0700

    Migrates `site` home page example test from Puppeteer to WebDriverIO.

--- examples/site/home/home_test.ts
+++ examples/site/home/home_test.ts
@@ -2,19 +2,18 @@ import 'jasmine';
 
 import { runfiles } from '@bazel/runfiles';
 import { useDevserver } from 'rules_prerender/common/testing/devserver';
-import { puppeteerTestTimeout, useBrowser, usePage } from 'rules_prerender/common/testing/puppeteer';
+import { useWebDriver, webDriverTestTimeout } from 'rules_prerender/common/testing/webdriver';
 
 const devserverBinary = runfiles.resolvePackageRelative('home_devserver');
 
 describe('home', () => {
     const devserver = useDevserver(devserverBinary);
-    const browser = useBrowser();
-    const page = usePage(browser);
+    const wd = useWebDriver(devserver);
 
     it('renders the home page', async () => {
-        await page.get().goto(
-            `http://${devserver.get().host}:${devserver.get().port}`);
-        
-        await expectAsync(page.get().title()).toBeResolvedTo('Home');
-    }, puppeteerTestTimeout);
+        const browser = wd.get();
+        await browser.url('/');
+
+        expect(await browser.getTitle()).toBe('Home');
+    }, webDriverTestTimeout);
 });
