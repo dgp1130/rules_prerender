62ed31dff79cf632c31af8972f096bd3723cfe25
Doug Parker <dgp1130@users.noreply.github.com>
2021-10-30 19:09:12 -0700

    Migrates `site` counter example test from Puppeteer to WebDriverIO.
    This is nice example of how the convenience of WebDriverIO's shadow DOM implementation makes the test much easier to write and clearer about its intent.

--- examples/site/components/counter/counter_test.ts
+++ examples/site/components/counter/counter_test.ts
@@ -2,103 +2,68 @@ import 'jasmine';
 
 import { runfiles } from '@bazel/runfiles';
 import { useDevserver } from 'rules_prerender/common/testing/devserver';
-import { puppeteerTestTimeout, useBrowser, usePage } from 'rules_prerender/common/testing/puppeteer';
+import { useWebDriver, webDriverTestTimeout } from 'rules_prerender/common/testing/webdriver';
 
 const devserverBinary =
     runfiles.resolvePackageRelative('counter_test_cases_devserver');
 
 describe('Counter', () => {
     const devserver = useDevserver(devserverBinary);
-    const browser = useBrowser();
-    const page = usePage(browser);
+    const wd = useWebDriver(devserver);
 
     it('increments when the increment button is clicked', async () => {
-        await page.get().goto(
-            `http://${devserver.get().host}:${devserver.get().port}/zero.html`,
-            { waitUntil: 'load' },
-        );
+        const browser = wd.get();
+        await browser.url('/zero.html');
 
-        const counter = (await page.get()
-            .evaluateHandle(`document.querySelector('site-counter').shadowRoot`)
-        ).asElement()!;
+        const counter = await browser.$('site-counter');
+        const label = await counter.shadow$('[label]');
 
-        const initialLbl =
-            await counter.$eval('[label]', (el) => el.textContent);
-        expect(initialLbl).toBe('The current count is: 0.');
-
-        const incrementBtn = (await counter.$('[increment]'))!;
-        await incrementBtn.click();
-
-        const incrementedLabel =
-            await counter.$eval('[label]', (el) => el.textContent);
-        expect(incrementedLabel).toBe('The current count is: 1.');
-    }, puppeteerTestTimeout);
+        expect(await label.getText()).toBe('The current count is: 0.');
+        await counter.shadow$('[increment]').click();
+        expect(await label.getText()).toBe('The current count is: 1.');
+    }, webDriverTestTimeout);
 
     it('decrements when the decrement button is clicked', async () => {
-        await page.get().goto(
-            `http://${devserver.get().host}:${devserver.get().port}/zero.html`,
-            { waitUntil: 'load' },
-        );
-        
-        const counter = (await page.get()
-            .evaluateHandle(`document.querySelector('site-counter').shadowRoot`)
-        ).asElement()!;
+        const browser = wd.get();
+        await browser.url('/zero.html');
 
-        const initialLbl =
-            await counter.$eval('[label]', (el) => el.textContent);
-        expect(initialLbl).toBe('The current count is: 0.');
+        const counter = await browser.$('site-counter');
+        const label = counter.shadow$('[label]');
 
-        const decrementBtn = (await counter.$('[decrement]'))!;
-        await decrementBtn.click();
-
-        const decrementedLabel =
-            await counter.$eval('[label]', (el) => el.textContent);
-        expect(decrementedLabel).toBe('The current count is: -1.');
-    }, puppeteerTestTimeout);
+        expect(await label.getText()).toBe('The current count is: 0.');
+        await counter.shadow$('[decrement]').click();
+        expect(await label.getText()).toBe('The current count is: -1.');
+    }, webDriverTestTimeout);
 
     it('hydrates from an initial positive value', async () => {
-        await page.get().goto(
-            `http://${devserver.get().host}:${devserver.get().port}/positive.html`,
-            { waitUntil: 'load' },
-        );
+        const browser = wd.get();
+        await browser.url('/positive.html');
         
-        const counter = (await page.get()
-            .evaluateHandle(`document.querySelector('site-counter').shadowRoot`)
-        ).asElement()!;
+        const counter = await browser.$('site-counter');
+        const label = await counter.shadow$('[label]');
 
-        const initialLbl =
-            await counter.$eval('[label]', (el) => el.textContent);
-        expect(initialLbl).toBe('The current count is: 4.');
+        // Assert the prerendered value is present.
+        expect(await label.getText()).toBe('The current count is: 4.');
 
-        // Increment to confirm that the initialized value is actually used.
-        const incrementBtn = (await counter.$('[increment]'))!;
-        await incrementBtn.click();
-
-        const incrementedLabel =
-            await counter.$eval('[label]', (el) => el.textContent);
-        expect(incrementedLabel).toBe('The current count is: 5.');
-    }, puppeteerTestTimeout);
+        // Increment to confirm that the initialized value is hydrated and used
+        // in client-side scripts.
+        await counter.shadow$('[increment]').click();
+        expect(await label.getText()).toBe('The current count is: 5.');
+    }, webDriverTestTimeout);
 
     it('hydrates from an initial negative value', async () => {
-        await page.get().goto(
-            `http://${devserver.get().host}:${devserver.get().port}/negative.html`,
-            { waitUntil: 'load' },
-        );
-        
-        const counter = (await page.get()
-            .evaluateHandle(`document.querySelector('site-counter').shadowRoot`)
-        ).asElement()!;
+        const browser = wd.get();
+        await browser.url('/negative.html');
 
-        const initialLbl =
-            await counter.$eval('[label]', (el) => el.textContent);
-        expect(initialLbl).toBe('The current count is: -7.');
+        const counter = browser.$('site-counter');
+        const label = counter.shadow$('[label]');
 
-        // Increment to confirm that the initialized value is actually used.
-        const incrementBtn = (await counter.$('[increment]'))!;
-        await incrementBtn.click();
+        // Assert the prerendered value is present.
+        expect(await label.getText()).toBe('The current count is: -7.');
 
-        const incrementedLabel =
-            await counter.$eval('[label]', (el) => el.textContent);
-        expect(incrementedLabel).toBe('The current count is: -6.');
-    }, puppeteerTestTimeout);
+        // Increment to confirm that the initialized value is hydrated and used
+        // in client-side scripts.
+        await counter.shadow$('[increment]').click();
+        expect(await label.getText()).toBe('The current count is: -6.');
+    }, webDriverTestTimeout);
 });
