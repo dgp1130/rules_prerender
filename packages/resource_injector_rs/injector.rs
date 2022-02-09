59d694c74c303c16c2e622a8f1765b62c23b5fdd
Doug Parker <dgp1130@users.noreply.github.com>
2022-02-08 21:01:29 -0800

    Injects `<style />` tag for requested styles.

--- packages/resource_injector_rs/injector.rs
+++ packages/resource_injector_rs/injector.rs
@@ -2,18 +2,18 @@ extern crate futures;
 #[macro_use]
 extern crate html5ever;
 extern crate markup5ever_rcdom as rcdom;
+extern crate tokio;
 
 use std::cell::RefCell;
 use std::error::Error;
 use std::future::Future;
 use std::pin::Pin;
 use std::rc::Rc;
-use html5ever::{Attribute, Namespace, QualName, parse_document, serialize};
+use html5ever::{Attribute, QualName, parse_document, serialize};
 use html5ever::tendril::{Tendril, TendrilSink};
 use rcdom::{Node, NodeData, RcDom, SerializableHandle};
 use config::{InjectorAction, InjectorConfig};
-
-static W3_NAMESPACE: &str = "http://www.w3.org/1999/xhtml";
+use tokio::fs;
 
 pub fn inject(html: String, config: InjectorConfig) -> Pin<Box<dyn Future<Output = Result<String, Box<dyn Error>>>>> {
     Box::pin(async move {
@@ -30,17 +30,14 @@ pub fn inject(html: String, config: InjectorConfig) -> Pin<Box<dyn Future<Output
             todo!()
         };
 
-        eprintln!("Document: {:?}", &dom.document); // DEBUG
-
         // Inject the requested resources.
         for action in config {
             match action {
                 InjectorAction::Script { path } => {
                     inject_script(&head, &path);
-                    // eprintln!("Head2: {:?}", &head); // DEBUG
                 },
                 InjectorAction::Style { path } => {
-                    inject_style(&head, &path);
+                    inject_style(&head, &path).await?;
                 },
             };
         }
@@ -76,8 +73,24 @@ fn inject_script(head: &Rc<Node>, path: &str) {
     }));
 }
 
-fn inject_style(head: &Rc<Node>, path: &str) {
-    // TODO
+async fn inject_style(head: &Rc<Node>, path: &str) -> Result<(), Box<dyn Error>> {
+    // Read the contents of the CSS file.
+    let css = fs::read_to_string(&path).await?;
+
+    // Create a `<style />` tag with the CSS contents inlined.
+    let style = Node::new(NodeData::Element {
+        name: QualName::new(None /* prefix */, ns!(), local_name!("style")),
+        attrs: RefCell::new(vec![]),
+        mathml_annotation_xml_integration_point: false,
+        template_contents: None,
+    });
+    style.children.borrow_mut().push(Node::new(NodeData::Text {
+        contents: RefCell::new(Tendril::from(css)),
+    }));
+
+    // Append the `<style />` tag to the `<head />` tag.
+    head.children.borrow_mut().push(style);
+    Ok(())
 }
 
 fn get_head(root: Rc<Node>) -> Option<Rc<Node>> {
