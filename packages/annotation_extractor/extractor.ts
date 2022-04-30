d46e7e145bea50f2f26b52a71436cef7f5b24bf0
Doug Parker <dgp1130@users.noreply.github.com>
2022-04-30 15:54:23 -0700

    Refactors `extractor.ts` to use `prerender_annotation_walker.ts`.
    This greatly simplifies the extractor and moves most of its functionality to a common location which can be reused.

--- packages/annotation_extractor/extractor.ts
+++ packages/annotation_extractor/extractor.ts
@@ -1,5 +1,6 @@
-import { parse, HTMLElement, Node, CommentNode } from 'node-html-parser';
-import { PrerenderAnnotation, parseAnnotation } from 'rules_prerender/common/models/prerender_annotation';
+import { parse, HTMLElement } from 'node-html-parser';
+import { PrerenderAnnotation } from 'rules_prerender/common/models/prerender_annotation';
+import { walkAllAnnotations } from 'rules_prerender/common/prerender_annotation_walker';
 
 /**
  * Parses the given string as HTML and return a tuple of the input HTML with
@@ -11,8 +12,8 @@ import { PrerenderAnnotation, parseAnnotation } from 'rules_prerender/common/mod
  *     parsed annotations found within the input HTML.
  */
 export function extract(html: string): [
-    string /* strippedHtml */,
-    Array<PrerenderAnnotation> /* annotations */,
+    strippedHtml: string,
+    annotations: PrerenderAnnotation[],
 ] {
     const root = parse(html, {
         comment: true,
@@ -26,52 +27,19 @@ export function extract(html: string): [
 
     // Read all annotations from the HTML **and** remove the comments they come
     // from. This actually applies a side effect of modifying the document.
-    const annotations = Array.from(stripAnnotations(walkComments(walk(root))));
+    const annotations = Array.from(stripAnnotations(root));
 
     return [ root.toString(), annotations ];
 }
 
 /**
- * Checks each given comment to see if it is an annotation. If so, the comment
- * is removed from the document and the annotation is yielded. Otherwise, the
- * comment is left alone and nothing is emitted.
+ * Removes all annotations under the given root node (except inline styles) and
+ * returns them.
  */
-function* stripAnnotations(
-    comments: Iterable<[ comment: CommentNode, parent: HTMLElement | undefined ]>,
-): Iterable<PrerenderAnnotation> {
-    for (const [ comment, parent ] of comments) {
-        const annotation = parseAnnotation(comment.text);
-        if (!annotation) continue;
-        if (!parent) throw new Error('Found comment node with no parent.');
-        parent.removeChild(comment);
-        yield annotation;
-    }
-}
-
-/**
- * Filters the given {@link Iterable} of {@link Node} to limit to only comment
- * nodes.
- */
-function* walkComments(
-    nodes: Iterable<[ node: Node, parent: HTMLElement | undefined ]>,
-): Iterable<[ comment: CommentNode, parent: HTMLElement | undefined ]> {
-    for (const [ node, parent ] of nodes) {
-        if (node instanceof CommentNode) {
-            yield [ node, parent ];
-        }
-    }
-}
-
-/**
- * Returns an {@link Iterable} of all the {@link Node} descendants of the given
- * root and their parent.
- */
-function* walk(root: Node, parent?: HTMLElement):
-        Iterable<[ node: Node, parent: HTMLElement | undefined ]> {
-    yield [ root, parent ];
-    if (root instanceof HTMLElement) {
-        for (const node of root.childNodes) {
-            yield* walk(node, root);
-        }
+function* stripAnnotations(root: HTMLElement):
+        Generator<PrerenderAnnotation, void, void> {
+    for (const node of walkAllAnnotations(root)) {
+        node.remove();
+        yield node.annotation;
     }
 }
