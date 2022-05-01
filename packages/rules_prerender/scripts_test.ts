310cf7a66bd02aaee3e1b14f2dde8ccc5f8b4816
Doug Parker <dgp1130@users.noreply.github.com>
2022-04-30 21:40:48 -0700

    Refactors `scripts_test` to use `createAnnotation()`.

--- packages/rules_prerender/scripts_test.ts
+++ packages/rules_prerender/scripts_test.ts
@@ -1,5 +1,6 @@
 import 'jasmine';
 
+import { createAnnotation } from 'rules_prerender/common/models/prerender_annotation';
 import { includeScript } from 'rules_prerender/packages/rules_prerender/scripts';
 
 describe('scripts', () => {
@@ -7,8 +8,10 @@ describe('scripts', () => {
         it('creates an annotation to include the given script', () => {
             const annotation = includeScript('./foo/bar/baz.js');
 
-            expect(annotation)
-                .toBe(`<!-- bazel:rules_prerender:PRIVATE_DO_NOT_DEPEND_OR_ELSE - {"type":"script","path":"./foo/bar/baz.js"} -->`);
+            expect(annotation).toBe(`<!-- ${createAnnotation({
+                type: 'script',
+                path: './foo/bar/baz.js',
+            })} -->`);
         });
     });
 });
