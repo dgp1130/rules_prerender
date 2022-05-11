ac7296d157ab3dbad7ef2af5aecc2935d9df8b1b
Doug Parker <dgp1130@users.noreply.github.com>
2022-05-10 21:52:16 -0700

    WIP: Adds basic resolver.

--- packages/css_bundler/css_bundler.rs
+++ packages/css_bundler/css_bundler.rs
@@ -1,3 +1,48 @@
+use std::env;
+use std::io;
+use std::path::Path;
+use std::process;
+use parcel_css::bundler::{Bundler, FileProvider, SourceProvider};
+use parcel_css::printer::PrinterOptions;
+use parcel_css::stylesheet::ParserOptions;
+
 fn main() {
-    println!("Hello, World!");
+    let args = parse_args(env::args().collect());
+
+    let fs = BazelFileProvider::new();
+    let mut bundler = Bundler::new(&fs, None, ParserOptions::default());
+    let stylesheet = bundler.bundle(Path::new(&args.entry_point)).unwrap();
+    let css = stylesheet.to_css(PrinterOptions::default()).unwrap().code;
+
+    println!("{}", css);
+}
+
+struct BazelFileProvider {
+    fs: FileProvider,
+}
+
+impl BazelFileProvider {
+    pub fn new() -> BazelFileProvider {
+        BazelFileProvider { fs: FileProvider::new() }
+    }
+}
+
+impl SourceProvider for BazelFileProvider {
+    fn read<'a>(&'a self, file: &Path) -> io::Result<&'a str> {
+        self.fs.read(&file.with_extension("css2"))
+    }
+}
+
+fn parse_args(args: Vec<String>) -> Args {
+    if args.len() < 2 {
+        eprintln!("Missing entry point argument.");
+        process::exit(1);
+    }
+    let entry_point = &args[1];
+
+    Args { entry_point: entry_point.to_string() }
+}
+
+struct Args {
+    entry_point: String,
 }
