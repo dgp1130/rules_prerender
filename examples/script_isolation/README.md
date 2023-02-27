0d3c4b287bd015a0d9d8e440862ce0550d373e14
Doug Parker <dgp1130@users.noreply.github.com>
2023-02-26 23:35:26 -0800

    Adds script isolation example.
    Refs #26.
    
    This verifies that scripts are only included on pages which actually reference them via `includeScript()`. It ensures that scripts which are _not_ included by the developer are _actually_ not included at runtime.

---
+++ examples/script_isolation/README.md
@@ -0,0 +1,5 @@
+# Script Isolation
+
+An example which bundles and loads JavaScript on multiple pages and verifies
+that only necessary scripts are included on the pages they are required. Scripts
+used on one page should not be forced on other pages.
---
+++ examples/script_isolation/site.ts
@@ -0,0 +1,49 @@
+import { includeScript, PrerenderResource } from 'rules_prerender';
+
+export default function*(): Generator<PrerenderResource, void, void> {
+    yield PrerenderResource.of('/index.html', `
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Script Isolation</title>
+        <meta charset="utf8">
+    </head>
+    <body>
+        <ul>
+            <li><a href="/foo.html">Foo</a></li>
+            <li><a href="/bar.html">Bar</a></li>
+        </ul>
+    </body>
+</html>
+    `.trim());
+
+    yield PrerenderResource.of('/foo.html', `
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Script Isolation</title>
+        <meta charset="utf8">
+    </head>
+    <body>
+        <div id="replace-foo">Text to be replaced.</div>
+        <div id="replace-bar">Text to be replaced.</div>
+        ${includeScript('examples/script_isolation/foo')}
+    </body>
+</html>
+    `.trim());
+
+    yield PrerenderResource.of('/bar.html', `
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Script Isolation</title>
+        <meta charset="utf8">
+    </head>
+    <body>
+        <div id="replace-foo">Text to be replaced.</div>
+        <div id="replace-bar">Text to be replaced.</div>
+        ${includeScript('examples/script_isolation/bar')}
+    </body>
+</html>
+    `.trim());
+}
