b3d24312a56f358eefad2c68331af86e030b64c1
Doug Parker <dgp1130@users.noreply.github.com>
2023-02-28 22:33:13 -0800

    Adds `FileSystem` and fake implementation.
    Refs #33.
    
    This is useful to mock and stub the file system without relying on spying on imports in Jasmine, which doesn't work with ESM.
    
    I investigated a few existing "fake file system" dependencies, however I wasn't really happy with how any of them worked or the supply chain implications of using them. I'm hoping to keep file system usage simple enough that it won't grow to be a maintenance headache, but we'll see how things shake out.

---
+++ common/file_system.ts
@@ -0,0 +1,54 @@
+import { promises as fs } from 'fs';
+
+/**
+ * A simplified file system interface, based on the NodeJs `fs` module with
+ * more limited features. This allows it to be easily implemented in multiple
+ * ways without pulling in all the complexity of the `fs` module's contracts.
+ */
+export interface FileSystem {
+    /** Copies a file from the source to the destination. */
+    copyFile(src: string, dest: string): Promise<void>;
+
+    /**
+     * Creates a new directory at the given location. Optionally creates all the
+     * parents of the directory.
+     */
+    mkdir(dir: string, options?: { recursive?: boolean }): Promise<void>;
+
+    /**
+     * Reads a file at the given path and returns it as a UTF8-encoded string or
+     * a binary buffer.
+     */
+    readFile(path: string, options: 'utf8'): Promise<string>;
+    readFile(path: string, options: { encoding: 'utf8' }): Promise<string>;
+    readFile(path: string, options?: string | { encoding?: string }):
+        Promise<string | Buffer>;
+}
+
+/** A file system implemented with the Node.js `fs` APIs. */
+class DiskFs implements FileSystem {
+    public async copyFile(src: string, dest: string): Promise<void> {
+        await fs.copyFile(src, dest);
+    }
+
+    public async mkdir(dir: string, options?: { recursive?: boolean }):
+            Promise<void> {
+        await fs.mkdir(dir, options);
+    }
+
+    readFile(path: string, options: 'utf8'): Promise<string>;
+    readFile(path: string, options: { encoding: 'utf8' }): Promise<string>;
+    readFile(path: string, options?: string | { encoding?: string }):
+        Promise<string | Buffer>;
+    public async readFile(path: string, options?: 'utf8' | { encoding?: string }):
+            Promise<string | Buffer> {
+        return await fs.readFile(
+            path,
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            options as any,
+        ) as unknown as Promise<string | Buffer>;
+    }
+}
+
+/** Singleton instance of the real disk file system. */
+export const diskFs = new DiskFs();
