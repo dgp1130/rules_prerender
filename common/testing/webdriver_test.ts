0023c30aa6cc9cd3a2ea4df8fb9257acd58745a4
Doug Parker <dgp1130@users.noreply.github.com>
2023-03-03 23:28:37 -0800

    Removes spy assertions from WebDriver test.
    Refs #33.
    
    We cannot spy on imported modules in native ESM, and we don't actually need those assertions anyways since we were calling through to a real WebDriver instance and actually invoking commands on a web page. The spies are wholly unnecessary to these tests.

--- common/testing/webdriver_test.ts
+++ common/testing/webdriver_test.ts
@@ -1,4 +1,3 @@
-import * as webdriverio from 'webdriverio';
 import { useDevserver } from './devserver';
 import { EffectTester } from './effect_tester';
 import { useWebDriver, webDriverTestTimeout } from './webdriver';
@@ -8,28 +7,11 @@ describe('webdriver', () => {
         const devserver = useDevserver('common/testing/webdriver_test_server.sh');
 
         it('manages a WebDriver session without a server under test', async () => {
-            const remoteSpy = spyOn(webdriverio, 'remote').and.callThrough();
-
             const tester = EffectTester.of(() => useWebDriver());
-            expect(remoteSpy).not.toHaveBeenCalled(); // Yet.
 
             // `.initialize()` starts a browser with the remote WebDriver server
             // instance.
             await tester.initialize();
-            expect(remoteSpy).toHaveBeenCalledTimes(1);
-            const options = remoteSpy.calls.first().args[0];
-            expect(options).toEqual({
-                // From non-deterministic WebDriver server environment variable.
-                hostname: jasmine.any(String),
-                port: jasmine.any(Number),
-                path: jasmine.any(String),
-
-                baseUrl: undefined, // No server under test to infer from.
-
-                capabilities: {
-                    browserName: 'chrome',
-                },
-            });
 
             // Verify connection to the devserver.
             const browser = tester.get();
@@ -41,28 +23,11 @@ describe('webdriver', () => {
         }, webDriverTestTimeout);
 
         it('manages a WebDriver session with a server under test', async () => {
-            const remoteSpy = spyOn(webdriverio, 'remote').and.callThrough();
-
             const tester = EffectTester.of(() => useWebDriver(devserver));
-            expect(remoteSpy).not.toHaveBeenCalled(); // Yet.
 
             // `.initialize()` starts a browser with the remote WebDriver server
             // instance.
             await tester.initialize();
-            expect(remoteSpy).toHaveBeenCalledTimes(1);
-            const options = remoteSpy.calls.first().args[0];
-            expect(options).toEqual({
-                // From non-deterministic WebDriver server environment variable.
-                hostname: jasmine.any(String),
-                port: jasmine.any(Number),
-                path: jasmine.any(String),
-
-                baseUrl: devserver.get().url,
-
-                capabilities: {
-                    browserName: 'chrome',
-                },
-            });
 
             // Verify connection to the devserver.
             const browser = tester.get();
