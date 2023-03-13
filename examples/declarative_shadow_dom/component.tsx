b6724c7fde0c386df9355ece8ef27b883f82a05c
Doug Parker <dgp1130@users.noreply.github.com>
2023-03-12 19:18:26 -0700

    Migrates `examples/declarative_shadow_dom/` to use Preact.
    Refs #71.

---
+++ examples/declarative_shadow_dom/component.tsx
@@ -0,0 +1,32 @@
+import { ComponentChildren, VNode } from 'preact';
+import { polyfillDeclarativeShadowDom } from '@rules_prerender/declarative_shadow_dom/preact.mjs';
+import { Template, inlineStyle } from '@rules_prerender/preact';
+
+/** Renders an example component using Declarative Shadow DOM. */
+export function Component({ children }: {
+    children: ComponentChildren,
+}): VNode {
+    return (
+        <div id="component"> {/* Host element. */}
+            {/* Shadow root. */}
+            <Template shadowroot="open">
+                {/* Polyfill declarative shadow DOM for browsers that don't
+                support it yet. */}
+                {polyfillDeclarativeShadowDom()}
+
+                {/* Inline styles to apply them within this shadow root. */}
+                {inlineStyle('./component.css', import.meta)}
+
+                {/* Shadow DOM content, styled with the associated style
+                sheet. */}
+                <div>Shadow content</div>
+
+                {/* Slot to insert the associated light DOM. */}
+                <slot></slot>
+            </Template>
+
+            {/* Light DOM content, not affected by above style sheet. */}
+            {children}
+        </div>
+    );
+}
--- examples/declarative_shadow_dom/site.tsx
+++ examples/declarative_shadow_dom/site.tsx
@@ -1,18 +1,17 @@
-import { PrerenderResource } from 'rules_prerender';
-import { renderComponent } from './component.mjs';
+import { PrerenderResource, renderToHtml } from '@rules_prerender/preact';
+import { Component } from './component.js';
 
 export default async function*(): AsyncGenerator<PrerenderResource, void, void> {
-    yield PrerenderResource.of('/index.html', `
-        <!DOCTYPE html>
+    yield PrerenderResource.of('/index.html', renderToHtml(
         <html>
             <head>
                 <title>Declarative Shadow DOM</title>
             </head>
             <body>
-                ${await renderComponent(`
+                <Component>
                     <div>Light content</div>
-                `)}
+                </Component>
             </body>
         </html>
-    `);
+    ));
 }
