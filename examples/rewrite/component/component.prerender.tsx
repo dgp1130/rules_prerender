f66b696a8870b1fc634be8ace8ffcef4b0404598
Doug Parker <dgp1130@users.noreply.github.com>
2023-07-21 21:35:39 -0700

    Adds script-only dependency to rewrite example.
    Refs #40.
    
    This dependency edge is only through a client-side script, not the prerender code. A client side script is pulled in from a dependency component which reads a text file. This text file is only included in the output if `@rules_prerender` is able to find it correctly. This verifies the new functionality of `prerender_component2` and confirms that script and style-only deps are supported.

--- examples/rewrite/component/component.prerender.tsx
+++ examples/rewrite/component/component.prerender.tsx
@@ -1,6 +1,5 @@
 import { polyfillDeclarativeShadowDom } from '@rules_prerender/declarative_shadow_dom/preact.mjs';
 import { Template, includeScript, inlineStyle } from '@rules_prerender/preact';
-import { Transitive } from '../transitive/transitive.prerender.js';
 import { VNode } from 'preact';
 
 export function Component({ name }: { name: string }): VNode {
@@ -8,13 +7,10 @@ export function Component({ name }: { name: string }): VNode {
         <Template shadowroot='open'>
             <img src="/logo.png" />
             <span>Hello to {name} from component!</span>
-            <slot></slot>
 
             {polyfillDeclarativeShadowDom()}
             {includeScript('./component.client.mjs', import.meta)}
             {inlineStyle('./component.css', import.meta)}
         </Template>
-
-        <Transitive name={name} />
     </div>;
 }
