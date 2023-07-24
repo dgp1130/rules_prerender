4dbfb45ae41c42a2c7543249eaf4d8fca4206e5a
Doug Parker <dgp1130@users.noreply.github.com>
2023-07-23 18:16:39 -0700

    Documents and fixes `Layout` component children.

--- docs/components/layout/layout.tsx
+++ docs/components/layout/layout.tsx
@@ -1,7 +1,16 @@
-import { VNode } from 'preact';
+import { VNode, ComponentChildren } from 'preact';
 
-/** TODO */
-export function Layout({ title, children }: { title: string, children: VNode }): VNode {
+/**
+ * Renders the base layout for documentation. Most pages should use this.
+ * 
+ * @param title The title of the page.
+ * @param children Children to render under the `<main>` element. Callers should
+ *     *not* render `<main>` themselves, `Layout` will do it automatically.
+ */
+export function Layout({ title, children }: {
+    title: string,
+    children: ComponentChildren,
+}): VNode {
     return <html lang="en">
         <head>
             <meta charSet="utf8" />
