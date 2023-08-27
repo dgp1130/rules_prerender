47d5719be5e51ce6c4370c0484b97af595caf6de
Doug Parker <dgp1130@users.noreply.github.com>
2023-08-27 13:02:05 -0700

    Adds more detail to the under construction page.

--- docs/components/under_construction/under_construction.tsx
+++ docs/components/under_construction/under_construction.tsx
@@ -1,3 +1,4 @@
+import { Template, inlineStyle } from '@rules_prerender/preact';
 import { VNode } from 'preact';
 import { Layout } from '../layout/layout.js';
 import { Route } from '../../route.mjs';
@@ -13,6 +14,18 @@ export function UnderConstruction({ pageTitle, headerTitle, routes } : {
         headerTitle={headerTitle}
         routes={routes}
     >
-        <div>This page is under construction.</div>
+        <div>
+            <Template shadowrootmode="open">
+                <h2>This page is under construction.</h2>
+
+                <p>
+                    Please check back later for this content. Alternatively, you
+                    can also make an issue or discussion <a href="https://github.com/dgp1130/rules_prerender/" rel="noopener" target="_blank">on GitHub</a> as well as
+                    reach out to the maintainer <a href="https://blog.dwac.dev/social/" rel="noopener" target="_blank">directly</a>.
+                </p>
+
+                {inlineStyle('./under_construction.css', import.meta)}
+            </Template>
+        </div>
     </Layout>;
 }
